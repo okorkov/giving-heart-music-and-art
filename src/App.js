31e99c412e5d0751256f54872b91ad8647e3da65
okorkov <alex.okorkov@gmail.com>
2021-04-13 11:26:19 -0700

    initial firebase deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Website is Under Construction
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <footer className="footer text-lg-start">
+    <div className="text-white text-center p-3" >
+      <a href="https://aokarkau.com" target="_blank"><p>{new Date().getFullYear()} © Alex Okarkau</p></a>
+    </div>
+  </footer>
     </div>
   );
 }
